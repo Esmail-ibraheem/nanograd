87adf681dca52973903bf212a959cece3c517f2d
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-07-31 10:58:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nanograd.models.GPT import inference_gpt
 from nanograd.models import ollama
 from nanograd.models import chat
    
-from nanograd.optimizers import AdamW  
+from nanograd.optimizers import AdamW   
 from nanograd.models.GPT import tokenizer 
 from nanograd.analysis_lab import sentiment_analysis
 
