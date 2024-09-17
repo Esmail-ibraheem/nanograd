2e58dbbd8edd4d662872e12d933409847d335b56
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-09-17 18:15:13 +0300

    create the autotrainer

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: Install the main project in editable mode
+pip install -e .
+
+:: Navigate to the 'llamafactory' directory and install it in editable mode
+cd nanograd\trainer
+pip install --no-deps -e .
+
+:: Navigate back to the original directory
+cd ..
