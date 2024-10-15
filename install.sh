d7c9842a20ec9587f990c1f8924a0f4bffd53d90
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-10-15 10:37:48 +0300

    linux and mac version

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install the main project in editable mode
+pip install -e .
+
+# Navigate to the 'nanograd/trainer' directory and install it in editable mode
+cd nanograd/trainer
+pip install --no-deps -e .
+
+# Navigate back to the original directory
+cd ../..
