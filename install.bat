0ab8e1b7754edd29a24951a8727dce03291d1089
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-10-15 10:38:26 +0300

    windows version

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 
 :: Install the main project in editable mode 
 pip install -e .
-
+ 
 :: Navigate to the 'llamafactory' directory and install it in editable mode 
 cd nanograd\trainer
 pip install --no-deps -e .
