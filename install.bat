b649f27dd5b55fb0930f4cf6fb95e9a6b5005651
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-10-15 10:35:54 +0300

    autotrainer installing

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: Install the main project in editable mode
+:: Install the main project in editable mode 
 pip install -e .
 
 :: Navigate to the 'llamafactory' directory and install it in editable mode 
